7a0223eb0b332ab5f6bec584ad5bd74481c0c250
Ramchandra Mahadev More <rammore248@gmail.com>
2025-01-02 17:09:06 +0530

    Custom UIButton and UITextField

---
+++ gitHubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  gitHubFollowers
+//
+//  Created by XTSW-MAC-1137-2 on 02/01/25.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
