f1cdb665cc1deee9f774b4623b307fe4f7b8eabf
Ramchandra Mahadev More <rammore248@gmail.com>
2025-01-03 12:29:43 +0530

    UI Search View and Pass Data Between Views

---
+++ gitHubFollowers/FollowerListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FollowerListVC.swift
+//  gitHubFollowers
+//
+//  Created by XTSW-MAC-1137-2 on 03/01/25.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
